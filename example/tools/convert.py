0d2e88372547998f5b705a0d4aa1c582c4bb2ceb
Sebastien Caps <493313+virer@users.noreply.github.com>
2023-08-30 12:10:32 +0200

    Added more example

--- example/tools/convert.py
+++ example/tools/convert.py
@@ -1,13 +1,28 @@
 from datetime import datetime
 import pandas as pd
 
+# Examples
+# file_in='gazoline.csv'
+# column_order=["value1","date"]
+# separator=';'
 
-df = pd.read_csv('gazoline.csv',sep=';', names=["value1","date"])
+# file_in = 'water.csv'
+# column_order = ["date","value1"]
+# separator = "\t" # Tab or ';' or ','
+# file_out = 'water.json'
+
+file_in = 'elec.csv'
+column_order = ["date","value1","value2"]
+separator = "\t" # Tab or ';' or ','
+file_out = 'elec.json'
+
+# ********************
+df = pd.read_csv(file_in, sep=separator, names=column_order)
+
 df['date'] = pd.to_datetime(df['date'], format='%d-%m-%y')
 df['date'] = df['date'].dt.strftime('%Y-%m-%d')
 df.set_index('date')
-# datetime.strptime(mydate, "%Y-%m-%d")
 
-with open('gazoline.json', 'w', encoding='utf-8') as file:
+with open(file_out, 'w', encoding='utf-8') as file:
     df.to_json(file, force_ascii=False, orient='records', index=True, indent=4)
 
